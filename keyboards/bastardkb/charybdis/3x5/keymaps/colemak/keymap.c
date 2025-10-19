33a32c5960f03eb8a1dd18dc96e2ee2f13fa250e
kevindeforth <32777623+kevindeforth@users.noreply.github.com>
2025-10-19 22:12:28 +0200

    no fucking via and it works

--- keyboards/bastardkb/charybdis/3x5/keymaps/colemak/keymap.c
+++ keyboards/bastardkb/charybdis/3x5/keymaps/colemak/keymap.c
@@ -162,7 +162,7 @@ static uint16_t auto_pointer_layer_timer = 0;
 // clang-format off
 /** \brief Colemak layout (3 rows, 10 columns). */
 #define LAYOUT_LAYER_BASE                                                                     \
-       KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,        KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, \
+       KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,        KC_J,    KC_L,    KC_U,    KC_Y,    KC_SEMICOLON, \
        KC_A,    KC_R,    KC_S,    KC_T,    KC_D,        KC_H,    KC_N,    KC_E,    KC_I,    KC_O,   \
        KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,        KC_K,    KC_M, KC_COMM,  KC_DOT,    KC_SLSH,\
                          TO_SYM,  KC_SPC,  TO_PTR,      KC_ENT,  KC_NO
